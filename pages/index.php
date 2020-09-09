f7875f66fc19b18b61dff7f34f2cb8f831626f77
borntohtmlcss <borntohtmlcss@gmail.com>
2020-09-09 11:08:21 +0530

    updated

---
+++ pages/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>MedTech Solution</title>
+<?php include('..\widgets\all_links.php');?>
+
+</head>
+<body>
+
+<!-- this is a navbar for before login -->
+<?php include('..\widgets\outer_navbar.php');?>
+
+<section class="home">
+    <div class="flex-parent">
+        <div class="box">
+            <p class="head"><span style="color:#8cc9f7">MedTech Solution</span> For a <span style="color:#4eeb68">healthy</span> society....</p>
+            <p class='myButton' ><a  href="get_started.php">Get Started</a></p>
+        </div>
+        <div>
+            <img src="../assets/img/head.png" alt="Girl in a jacket" width="550" height="500">
+        </div>
+    </div>
+</section>
+
+
+</body>
+</html>
+

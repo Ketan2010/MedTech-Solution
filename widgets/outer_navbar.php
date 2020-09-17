70d9cb42da338ea5be0861432ded453890ccb4cf
borntohtmlcss <borntohtmlcss@gmail.com>
2020-09-17 17:26:57 +0530

    updated

--- widgets/outer_navbar.php
+++ widgets/outer_navbar.php
@@ -3,7 +3,7 @@
     <a href="../pages/index.php"  class="logo">MedTech Solution</a>
     <ul>
         <li class='myButton' ><a href="../pages/index.php">Home</a></li>
-        <li class='myButton'><a href="#">Health Passport</a></li>
+        <li class='myButton'><a href="../Health_Passport/health_passport_login.php">Health Passport</a></li>
         <li class='myButton'><a href="#">Admin Login</a></li>
     </ul>
 </header>

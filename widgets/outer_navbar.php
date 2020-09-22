adbbcf5d0b58858b0192e8db40411e494ef1367c
Ketan Patil <k2p1020@gmail.com>
2020-09-22 20:10:29 +0530

    Delete outer_navbar.php

--- widgets/outer_navbar.php
+++
@@ -1,9 +0,0 @@
-<!-- navbar before login -->
-<header class='nav'>
-    <a href="../pages/index.php"  class="logo">MedTech Solution</a>
-    <ul>
-        <li class='myButton' ><a href="../pages/index.php">Home</a></li>
-        <li class='myButton'><a href="../Health_Passport/health_passport_login.php">Health Passport</a></li>
-        <li class='myButton'><a href="#">Admin Login</a></li>
-    </ul>
-</header>

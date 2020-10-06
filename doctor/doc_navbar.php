95230d25afebe7226427660ec05c0b7572ded061
Ketan Patil <k2p1020@gmail.com>
2020-10-06 19:23:50 +0530

    Authentication added on Login, registration forms.

--- doctor/doc_navbar.php
+++ doctor/doc_navbar.php
@@ -5,10 +5,10 @@
         <li style="margin: 0 0 0 400px;" class='myButton'><a href="#">Health Passport</a></li>
     </ul>
     <div class="dropdown">
-        <a class="dropbtn" class="logo"><span class="fas fa-user-md"></spaan> Dr. Joahn</a>
+        <a class="dropbtn" class="logo"><span class="fas fa-user-md"></spaan> Dr. <?php echo $dr?></a>
         <div class="dropdown-content">
             <a href="doc_profile.php"><span class="fas fa-user-circle"></spaan> Profile</a>
-            <a href="#"><span class="fas fa-sign-out-alt"></spaan> Logout</a>
+            <a href="doc_logout.php"><span class="fas fa-sign-out-alt"></spaan> Logout</a>
             
         </div>
     </div>

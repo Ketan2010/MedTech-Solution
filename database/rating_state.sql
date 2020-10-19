1220c47032ae08b896d2a1327b74cf8e56c8adae
Ketan Patil <k2p1020@gmail.com>
2020-10-19 19:17:33 +0530

    rating status added

---
+++ database/rating_state.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2020 at 03:44 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `intp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating_state`
+--
+
+CREATE TABLE `rating_state` (
+  `rt_id` int(200) NOT NULL,
+  `pat_id` int(200) NOT NULL,
+  `doc_id` int(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `rating_state`
+--
+ALTER TABLE `rating_state`
+  ADD PRIMARY KEY (`rt_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `rating_state`
+--
+ALTER TABLE `rating_state`
+  MODIFY `rt_id` int(200) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

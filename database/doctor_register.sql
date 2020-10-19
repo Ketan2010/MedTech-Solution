b05263222d088841edfe984e18cac3fbb6aaa45e
Ketan Patil <k2p1020@gmail.com>
2020-10-19 19:07:40 +0530

    Update doctor_register.sql

--- database/doctor_register.sql
+++ database/doctor_register.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2020 at 12:05 PM
+-- Generation Time: Oct 19, 2020 at 03:31 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.31
 
@@ -32,8 +32,20 @@ CREATE TABLE `doctor_register` (
   `doc_name` text NOT NULL,
   `doc_specializn` text NOT NULL,
   `doc_gender` text NOT NULL,
-  `doc_mail` text NOT NULL
+  `doc_mail` text NOT NULL,
+  `ratings` int(11) NOT NULL DEFAULT 3
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctor_register`
+--
+
+INSERT INTO `doctor_register` (`doc_id`, `doc_name`, `doc_specializn`, `doc_gender`, `doc_mail`, `ratings`) VALUES
+(1, 'Joan', 'Cradiologist', 'male', 'joan@doc.com', 2),
+(3, 'Shiv', 'MD', 'male', 'shiv@doc.com', 3),
+(4, 'July', 'Cradiologist', 'female', 'july@doc.com', 1),
+(5, 'Micky', 'Dental', 'female', 'micky@doc.com', 3),
+(6, 'Teena', 'Dental', 'male', 'teena@doc.com', 3);
 
 --
 -- Indexes for dumped tables
@@ -53,7 +65,7 @@ ALTER TABLE `doctor_register`
 -- AUTO_INCREMENT for table `doctor_register`
 --
 ALTER TABLE `doctor_register`
-  MODIFY `doc_id` int(200) NOT NULL AUTO_INCREMENT;
+  MODIFY `doc_id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

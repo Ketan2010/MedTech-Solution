83b045a3f436b32f6e3477dbcb4e6bfb14fd51f2
Ketan Patil <k2p1020@gmail.com>
2020-11-03 15:43:20 +0530

    version3

---
+++ get_started.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>MedTech Solution</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Tab logo -->
+<link href="assets/img/tab_logo.png" rel="icon">
+<!-- main js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="javascript/main.js"></script>
+<!-- Template Main CSS File --> 
+<link href="stylesheet/styleme.css" rel="stylesheet" type="text/css" media="screen, projection"/>
+<!-- google fonts -->
+<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+<!-- icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<!-- javascript pie chart -->
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</head>
+<body>
+<!-- this is a navbar for before login -->
+<header class='nav'>
+    <a href="index.php"  class="logo">MedTech Solution</a>
+    <ul>
+        <li ><a > </a></li>
+        <li ><a > </a></li>
+        <li ><a > </a></li>
+        <li class='myButton' ><a href="index.php">Home</a></li>
+        <li class='myButton'><a href="Health_Passport/health_passport_login.php">Health Passport</a></li>
+    </ul>
+</header>
+
+<section class="home">
+    <div class="flex-parent">
+        <!-- i m doctor -->
+        <div class="boxone">
+            <img src="assets/img/doc.png" alt="Doctor" width="400" height="400">
+            <p style="text-align: center;" class='myButton' ><a  href="doctor/doctor_login.php">I'm Doctor</a></p>
+        </div>
+        <!-- i m Patient -->
+        <div class="boxone">
+            <img src="assets/img/pat.png" alt="Patient" width="400" height="400">
+            <p style="text-align: center;" class='myButton' ><a  href="patient/patient_login.php">I'm Patient</a></p>
+        </div>
+    </div>
+</section>
+
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>MedTech Solution</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Tab logo -->
+<link href="assets/img/tab_logo.png" rel="icon">
+<!-- main js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="javascript/main.js"></script>
+<!-- Template Main CSS File --> 
+<link href="stylesheet/styleme.css" rel="stylesheet" type="text/css" media="screen, projection"/>
+<!-- google fonts -->
+<link href='https://fonts.googleapis.com/css?family=Sofia' rel='stylesheet'>
+<!-- icons -->
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<!-- javascript pie chart -->
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+</head>
+<body>
+
+<!-- this is a navbar for before login -->
+<header class='nav'>
+    <a href="index.php"  class="logo">MedTech Solution</a>
+    <ul>
+        <li ><a > </a></li>
+        <li ><a > </a></li>
+        <li ><a > </a></li>
+        <li class='myButton' ><a href="index.php">Home</a></li>
+        <li class='myButton'><a href="Health_Passport/health_passport_login.php">Health Passport</a></li>
+    </ul>
+</header>
+
+<section class="home">
+    <div class="flex-parent">
+        <div class="box">
+            <p class="head"><span style="color:#8cc9f7">MedTech Solution</span> For a <span style="color:#4eeb68">healthy</span> society....</p>
+            <p class='myButton' ><a  href="get_started.php">Get Started</a></p>
+        </div>
+        <div>
+            <img src="assets/img/head.png" alt="" width="550" height="500">
+        </div>
+    </div>
+</section>
+
+
+</body>
+</html>
+
